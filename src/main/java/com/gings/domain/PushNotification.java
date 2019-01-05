f675895527f1e1a632c0db3092c0615680c88020
seunghx <seunghx@gmail.com>
2019-01-06 00:07:12 +0900

    Add push notification related component

---
+++ src/main/java/com/gings/domain/PushNotification.java
@@ -0,0 +1,21 @@
+package com.gings.domain;
+
+import java.time.LocalDateTime;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class PushNotification {
+    
+    public int userId;
+    //public NotificationType notificationType;
+    public String NotificationType;
+    public String message;
+    public boolean confirmed;
+    public LocalDateTime time;
+    
+}
