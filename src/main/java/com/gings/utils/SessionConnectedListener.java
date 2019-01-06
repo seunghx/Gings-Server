4be959c558934a292f0a8a642882d68a81c6301c
seunghx <seunghx@gmail.com>
2019-01-07 00:24:01 +0900

    Polishing SessionConnectedListener

--- src/main/java/com/gings/utils/SessionConnectedListener.java
+++ src/main/java/com/gings/utils/SessionConnectedListener.java
@@ -12,8 +12,8 @@ import lombok.extern.slf4j.Slf4j;
 public class SessionConnectedListener implements ApplicationListener<SessionConnectedEvent> {
 
     // Methods
-    // ==========================================================================================================================
-
+    // =================================================================================================
+    
     @Override
     public void onApplicationEvent(SessionConnectedEvent event) {
         if(log.isInfoEnabled()) {
