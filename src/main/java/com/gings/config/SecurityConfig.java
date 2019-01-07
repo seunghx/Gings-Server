a58eb57fc5cea755a10f40691d1ef261a57bae4e
seunghx <seunghx@gmail.com>
2019-01-07 17:27:07 +0900

    Modify security config for StompConnectFilter

--- src/main/java/com/gings/config/SecurityConfig.java
+++ src/main/java/com/gings/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -60,9 +61,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     }
     
+    @Override
     @Bean
-    public StompConnectAuthenticationFilter stompConnectAuthenticationFilter() {
-        return new StompConnectAuthenticationFilter(connectRequestMatcher(), jwtServiceManager(), userMapper);
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+
+        return super.authenticationManagerBean();
+    }
+    
+    @Bean
+    public StompConnectAuthenticationFilter stompConnectAuthenticationFilter() throws Exception {
+        StompConnectAuthenticationFilter filter = 
+                                 new StompConnectAuthenticationFilter(connectRequestMatcher(), 
+                                                                      jwtServiceManager(), 
+                                                                      userMapper);
+        
+        filter.setAuthenticationManager(authenticationManagerBean());
+        return filter;
     }
     
     @Bean
