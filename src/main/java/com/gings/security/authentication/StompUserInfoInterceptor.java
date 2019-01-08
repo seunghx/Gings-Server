3fb1c6ad1a3579b8ea4478793ee485d2409a09c2
ywoo21 <ywooo21@gmail.com>
2019-01-08 21:48:23 +0900

    Add Search

--- src/main/java/com/gings/security/authentication/StompUserInfoInterceptor.java
+++ src/main/java/com/gings/security/authentication/StompUserInfoInterceptor.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 import com.gings.security.jwt.UserAuthTokenInfo;
 
-import jdk.internal.jline.internal.Log;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
