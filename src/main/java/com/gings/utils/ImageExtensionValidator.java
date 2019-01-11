bb679472e62f7c326930fe416d8d19a027cc9353
seunghx <seunghx@gmail.com>
2019-01-12 00:00:12 +0900

    Modify

--- src/main/java/com/gings/utils/ImageExtensionValidator.java
+++ src/main/java/com/gings/utils/ImageExtensionValidator.java
@@ -5,6 +5,8 @@ package com.gings.utils;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -22,7 +24,12 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class ImageExtensionValidator implements ConstraintValidator<ImageExtension, MultipartFile> {
 
-
+    private MessageSource msgSource;
+    
+    public ImageExtensionValidator(MessageSource msgSource) {
+        this.msgSource = mesgSource;
+    }
+    
     @Override
     public void initialize(ImageExtension contactNumber) {
     }
@@ -33,7 +40,8 @@ public class ImageExtensionValidator implements ConstraintValidator<ImageExtensi
         if(file == null) return true;
 
         log.debug("Delegating image file name validation to {}.", ImageOperationProvider.class);
-
+        
+ 
         try {
             ImageOperationProvider.validateImage(file.getOriginalFilename());
         } catch (UnsupportedImageFormatException ex) {
@@ -41,11 +49,15 @@ public class ImageExtensionValidator implements ConstraintValidator<ImageExtensi
                 log.info("Validating image file name failed due to {}.", ex.toString());
                 log.info("Invalid file name : {}", ex.getFileName());
             }
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(
+                    msgSource.getMessage("response.exception.multipart.invalid", null, LocaleContextHolder.getLocale()))
+                    .addConstraintViolation();
             return false;
         } catch (Throwable throwable) {
             log.error("{}", throwable);
         }
-
+        
         log.debug("Validating image file name succeeded.");
         
         return true;
